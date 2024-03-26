8ec63fbf301bfd2a5a2ef90b8a6e16c5859104ed
CleveyP <clevelandplonsey@gmail.com>
2024-03-26 02:09:18 -0700

    the board component maps out the board And the pieces in their initial state

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -2,24 +2,23 @@ import { View, StyleSheet } from "react-native";
 import {BoardSquare} from "../BoardSquare/BoardSquare";
 import {LETTERS} from  "../Board/board.js";
 import Pawn from "../../pieces/Pawn";
+import getStartingBoard from "../Board/board.js";
+import React, {useEffect, useState} from "react";
+
 export const Board = () => {
-    const blackSquareSource = require("../../assets/board/blacksquare.jpg");
-    const whiteSquareSource = require("../../assets/board/whitesquare.jpg");
-    console.log("inside board");
+    const [board, setBoard] = useState(getStartingBoard()); // 8x8 array
     const rows = [];
     for (let row = 1; row <= 8; row++) {
-
         const columns = [];
         for (let col = 1; col <= 8; col++) {
-            const isWhiteSquare = (row + col) % 2 === 0;
 
             columns.push(
                 <BoardSquare
                     key={`square-${row}-${col}`}
-                    src={isWhiteSquare ? whiteSquareSource : blackSquareSource}
-                    letter={LETTERS[col.toString()]}
-                    number={row}
-                    piece={new Pawn("black", "a", 1)}
+                    src={board[row-1][col-1].src}
+                    letter={board[row-1][col-1].letter}
+                    number={board[row-1][col-1].number}
+                    piece={board[row-1][col-1].piece}
                 />
             );
         }
--- pieces/King.js
+++ pieces/King.js
@@ -1,11 +1,12 @@
 import Piece from "./Piece.js";
-const whiteKingSource = require("../../assets/pieces/whiteking.jpg");
-const blackKingSource = require("../../assets/pieces/blackking.jpg");
+const whiteKingSource = "../assets/pieces1/wk.svg";
+const blackKingSource = "../assets/pieces1/bk.svg";
 
 class King extends Piece {
 
     constructor(color, letter, number) {
-        super(color, color == black ? blackKingSource : whiteKingSource, letter, number, "king");
+        const src = color === 'black' ? blackKingSource : whiteKingSource;
+        super(color, src, letter, number, "king");
     }
 
     isValidMove(destLetter, destNumber){
