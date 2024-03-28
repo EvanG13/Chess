f9fe0d5f0a8854042b83f6b3132694c6f40859af
EvanG13 <emgiampaoli@gmail.com>
2024-03-27 21:19:37 -0700

    chore: run prettier and linter (#6)

--- components/BoardSquare/BoardSquare.jsx
+++ components/BoardSquare/BoardSquare.jsx
@@ -1,35 +1,35 @@
-import {View, ImageBackground, StyleSheet, Image} from "react-native";
+import { View, ImageBackground, StyleSheet, Image } from "react-native";
 
 export const BoardSquare = ({ src, letter, number, piece }) => {
-    return (
-        <View style={styles.square}>
-            {piece!= null && <Image source={piece.src} style={styles.pieceImg}/>}
-            <ImageBackground source={src} style={styles.image} />
-        </View>
-    );
+  return (
+    <View style={styles.square}>
+      {piece != null && <Image source={piece.src} style={styles.pieceImg} />}
+      <ImageBackground source={src} style={styles.image} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    square: {
-        width: 45,
-        height: 45,
-        position: 'relative',
-    },
-    pieceImg: {
-        width: 45,
-        height: 45,
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        zIndex: 3,
-    },
-    image: {
-        flex: 1,
-        resizeMode: "cover",
-        position: 'absolute', 
-        top: 0,
-        left: 0,
-        width: '100%', 
-        height: '100%',
-    }
+  square: {
+    width: 45,
+    height: 45,
+    position: "relative",
+  },
+  pieceImg: {
+    width: 45,
+    height: 45,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: 3,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
 });
--- pieces/Rook.js
+++ pieces/Rook.js
@@ -1,19 +1,14 @@
-
 import Piece from "./Piece.js";
-const whiteRookSource =  require("../assets/gothicPieces/wr.png");
-const blackRookSource =  require("../assets/gothicPieces/br.png");
+const whiteRookSource = require("../assets/gothicPieces/wr.png");
+const blackRookSource = require("../assets/gothicPieces/br.png");
 
 class Rook extends Piece {
+  constructor(color, letter, number) {
+    const src = color === "black" ? blackRookSource : whiteRookSource;
+    super(color, src, letter, number, "pawn");
+  }
 
-    constructor(color, letter, number) {
-       const src = color === 'black' ? blackRookSource : whiteRookSource;
-        super(color, src, letter, number, "pawn");
-        
-    }
-
-    isValidMove(destLetter, destNumber){
-
-    }
+  isValidMove(destLetter, destNumber) {}
 }
 
 export default Rook;
