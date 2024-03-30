e08adc7c9a8ca9c47f9bdc32f9005d90c1edfb1c
Anjana Nambiar <anjana.n2308@gmail.com>
2024-03-29 19:18:26 -0700

    implemented getValidMoves() for knight

--- pieces/Knight.js
+++ pieces/Knight.js
@@ -8,6 +8,29 @@ class Knight extends Piece {
     super(color, src, letter, number, "knight");
   }
 
+  getValidMoves(board){
+    const col = this.convertLetterToNumber(this.letter);
+    const dirs = [[2,1],[2,-1],[1,2],[1,-2],[-2,1],[-2,-1],[-1,2],[-1,-2]];
+    let validMoves = [];
+    let newRow;
+    let newCol;
+     for (let i = 0; i < dirs.length; i++) {
+          newRow = this.number + dirs[i][0];
+          newCol = col + dirs[i][1];
+
+          if (newRow > 7 || newRow < 0 || newCol > 7 || newCol < 0) {
+            continue;
+          }
+
+          if (
+            board[newRow][newCol].piece === null ||
+            board[newRow][newCol].piece.color != this.color
+          ) {
+            validMoves.push([newRow, newCol]);
+          }
+        }
+        return validMoves;
+  }
   isValidMove(destLetter, destNumber) {
     const letterDifference = Math.abs(
       destLetter.charCodeAt(0) - this.letter.charCodeAt(0)
@@ -21,6 +44,7 @@ class Knight extends Piece {
     }
     return false;
   }
+
 }
 
 export default Knight;
