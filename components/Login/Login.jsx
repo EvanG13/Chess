8ad3f0f2626f9fd50151b88a5f2d0af6861ad6ad
CleveyP <clevelandplonsey@gmail.com>
2024-04-18 06:33:16 -0700

    style login and register components

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [username, setUsername] = useState("");
 
   const handleLogin = async () => {
     if (email === "" || password === "") {
@@ -14,19 +13,84 @@ const Login = () => {
     }
     //TODO: fetch request to backend
   };
-  return (
-    <View>
-      <Text>Login</Text>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
 
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Login" onPress={handleLogin} />
+  return (
+    <View style={styles.loginContainer}>
+      <View style={styles.loginCard}>
+        {/* Rainbow border */}
+        <LinearGradient
+          colors={[
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "indigo",
+            "violet"
+          ]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.gradient}
+        >
+          {/* Black background inside the card */}
+          <View style={styles.innerCard}>
+            <Text style={styles.loginHeader}>Login</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+            />
+
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+            <Button title="Login" onPress={handleLogin} />
+          </View>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  loginHeader: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "white"
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  loginCard: {
+    borderRadius: 10,
+    overflow: "hidden", // Ensure the inner gradient doesn't overflow
+    width: "60%"
+  },
+  gradient: {
+    flex: 1,
+    borderRadius: 10,
+    borderWidth: 2, // Add borderWidth to create a border
+    borderColor: "transparent" // Set initial borderColor to transparent
+  },
+  innerCard: {
+    backgroundColor: "black",
+    padding: 20
+  },
+  input: {
+    backgroundColor: "white",
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5
+  }
+});
 
 export default Login;
