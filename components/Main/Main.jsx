00ca2a9eb8a69003e5ded5d60f165b663e02c982
CleveyP <clevelandplonsey@gmail.com>
2024-04-18 07:46:18 -0700

    style the main page

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -16,22 +16,27 @@ const Main = ({ navigation }) => {
     <View style={styles.main}>
       <MainHeader navigation={navigation} />
       <View style={styles.mainBody}>
-        <Text>KysCleveEvan Chess</Text>
-        <Image source={greenPieces} style={styles.greenPieces} />
-        <Button
-          title="Start Local Game"
-          onPress={() => {
-            navigation.navigate("localGame");
-          }}
-        />
-        <TouchableOpacity
-          style={styles.tutorial}
-          onPress={() => {
-            navigation.navigate("tutorial");
-          }}
-        >
-          <Image source={tutorialImage} />
-        </TouchableOpacity>
+        <Text style={styles.siteHeader}>KysCleveEvan Chess</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-around", width: "100%"  }}>
+          <View style={{height: "80%", width: "40%", borderRadius: 10}}>
+            <Image source={greenPieces} style={styles.greenPieces} />
+            <Button
+              title="Start Local Game"
+              onPress={() => {
+                navigation.navigate("localGame");
+              }}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.tutorial}
+            onPress={() => {
+              navigation.navigate("tutorial");
+            }}
+          >
+            <Text style={{color: "white",  fontSize: 20}}>Chess Tutorial</Text>
+            <Image source={tutorialImage} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -39,28 +44,32 @@ const Main = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   main: {
-    backgroundColor: "white",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     height: "100%",
     width: "100%"
   },
+  siteHeader:{
+    fontSize: 30,
+    color: "white",
+    marginBottom: "5%"
+  },
   mainBody: {
     height: "80%",
     width: "100%",
-    backgroundColor: "white",
+    backgroundColor: "black",
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
   },
   greenPieces: {
-    width: "70%",
-    height: "45%"
+    width: "100%",
+    height: "100%"
   },
   tutorial: {
-    width: "70%",
-    height: "40%",
+    width: "50%",
+    height: "80%",
     marginBottom: "5%"
   }
 });
