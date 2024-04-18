c92e392ac8527ad3b5ad65fc05f77dd937ae1c8d
CleveyP <clevelandplonsey@gmail.com>
2024-04-18 03:11:25 -0700

    create login and register components. No styling

---
+++ components/Login/Login.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import {View, Text, TextInput, Button} from "react-native";
+
+const Login = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [username, setUsername] = useState('');
+
+    const handleLogin = async () =>{
+       if(email === '' || password === '')
+         {
+              alert('Please fill out all fields');
+              return;
+         }
+        //TODO: fetch request to backend
+    }
+    return (
+        <View>
+            <Text>Login</Text>
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                />
+       
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                />
+            <Button title="Login" onPress={handleLogin}/>
+        
+        </View>
+    )
+}
+
+export default Login;
---
+++ components/Register/Register.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import {View, Text, TextInput, Button} from "react-native";
+
+const Register = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [username, setUsername] = useState('');
+
+    const handleRegister = async () =>{
+        if(password !==  confirmPassword)
+        {
+            alert('Passwords do not match');
+            return;
+        }
+        //TODO: fetch request to backend
+    }
+    return (
+        <View>
+            <Text>Register</Text>
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                />
+            <TextInput 
+            placeholder="Username"
+            value = {username}
+            onChangeText={setUsername}
+            />
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                />
+            <TextInput
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                />
+            <Button title="Register" onPress={handleRegister}/>
+        </View>
+    )
+}
+
+export default Register
