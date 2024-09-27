ecd0b10e849a8034e5d75bb4089fa6a22b49fdd3
CleveyP <clevelandplonsey@gmail.com>
2024-09-27 16:28:05 -0700

    create handleSocket function

---
+++ components/Board/socketHandler.js
@@ -0,0 +1,23 @@
+import Actions from "../../types/Actions";
+
+
+const handleSocket = (event, setters) => {
+    console.log(event);
+    const {action, data} = event;
+    switch (action) {
+        case Actions.GAME_START:
+            console.log(data);
+            break;
+        case Actions.GAME_OVER:
+            console.log(data);
+            break;
+        case Actions.MOVE_MADE:
+            console.log(data);
+            break;
+        case Actions.CHAT_MESSAGE:
+            console.log(data);
+            break;
+        default:
+            console.log(data);
+    }
+}
