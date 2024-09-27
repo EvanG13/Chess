5c17fbd51ca7aaae805175592735956922bc6bce
CleveyP <clevelandplonsey@gmail.com>
2024-09-27 16:28:22 -0700

    create Actions enum and types folder

---
+++ types/Actions.js
@@ -0,0 +1,12 @@
+
+const Actions = Object.freeze({
+    GAME_START: 'GAME_START',
+    GAME_OVER: 'GAME_OVER',
+    MOVE_MADE: 'MOVE_MADE',
+    CHAT_MESSAGE: 'CHAT_MESSAGE',
+    
+});
+
+
+
+export default Actions;
