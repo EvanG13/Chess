fb757708d464142d7da4511f115922ea964782fb
CleveyP <clevelandplonsey@gmail.com>
2024-09-28 03:48:51 -0700

    chore: format files.

--- components/Board/socketHandler.js
+++ components/Board/socketHandler.js
@@ -4,21 +4,21 @@ const handleSocket = (event, setters) => {
   console.log(event);
   const { action, data } = event;
   switch (action) {
-  case Actions.GAME_START:
-    console.log(data);
-    setters.setIsStarted(true);
-    break;
-  case Actions.GAME_OVER:
-    console.log(data);
-    break;
-  case Actions.MOVE_MADE:
-    console.log(data);
-    break;
-  case Actions.CHAT_MESSAGE:
-    console.log(data);
-    break;
-  default:
-    console.log(data);
+    case Actions.GAME_START:
+      console.log(data);
+      setters.setIsStarted(true);
+      break;
+    case Actions.GAME_OVER:
+      console.log(data);
+      break;
+    case Actions.MOVE_MADE:
+      console.log(data);
+      break;
+    case Actions.CHAT_MESSAGE:
+      console.log(data);
+      break;
+    default:
+      console.log(data);
   }
 };
 
