2af0969cc10d242a026363688a28d16596e5896b
CleveyP <clevelandplonsey@gmail.com>
2024-09-30 03:59:39 -0700

    implement part of socketHandler

--- components/Board/socketHandler.js
+++ components/Board/socketHandler.js
@@ -1,18 +1,31 @@
 import Actions from "../../types/Actions";
-
+import fenToJSON from "./fenToJSON";
 const handleSocket = (event, setters) => {
-  console.log(event);
-  const { action, data } = event;
+ 
+  const { action, data } = JSON.parse(event.data);
+  console.log(action, data);
   switch (action) {
     case Actions.GAME_START:
       console.log(data);
       setters.setIsStarted(true);
+      //setters.setIsWhite()
+      sessionStorage.setItem("gameId", data.gameId);
       break;
     case Actions.GAME_OVER:
       console.log(data);
       break;
     case Actions.MOVE_MADE:
       console.log(data);
+      if (!data.isSuccess){
+        console.log("Invalid move");
+        return;
+      }
+      const fen = data.fen;
+      console.log(fen);
+      const boardJson = fenToJSON(fen);
+      const isWhiteTurn = data.isWhiteTurn;
+      setters.setBoard(boardJson);
+      setters.setIsWhiteTurn(isWhiteTurn);
       break;
     case Actions.GAME_CREATED:
       console.log(data);
