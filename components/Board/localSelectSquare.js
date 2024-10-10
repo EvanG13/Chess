1d41f29e3e9de12a4e0db0e59ecb84a3816cdbbf
Cleveland Plonsey <87806855+CleveyP@users.noreply.github.com>
2024-10-09 20:23:32 -0700

    46 refactor the selectsquare function (#48)
    * create localSelectSquare for local games. (need a local Board.jsx as well)
    
    * pull logic out of  selectSquare and into algorithm file
    
    * craete selectSquareAlg file and implement its methods
    
    * chore: format files
    
    * chore: lint files
    
    * remove logging logic from selectSquare
    
    * Board component correctly sets up game if user returns to game
    
    * chore: run fomat and lint

---
+++ components/Board/localSelectSquare.js
@@ -0,0 +1,195 @@
+import { getNumberFromLetter } from "../Board/board.js";
+import { LETTERS } from "../Board/board.js";
+import King from "../../pieces/King.js";
+import convertToChessNotation from "../Logger/toChessNotation.js";
+import { EmitActions } from "../../types/Actions.js";
+import {
+  isValidMove,
+  selectDifferentPiece,
+  selectNewPiece
+} from "./selectSquareAlgs.js";
+
+const localSelectSquare = (
+  number,
+  letter,
+  board,
+  setBoard,
+  selectedSquare,
+  setSelectedSquare,
+  isWhiteTurn,
+  setIsWhiteTurn,
+  validMoves,
+  setValidMoves,
+  kingSquare,
+  setKingSquare,
+  setHasWon,
+  hasWon,
+  setShowWinner,
+  setLog,
+  log,
+  setMoveIndex,
+  moveIndex,
+  socket,
+  isWhite
+) => {
+  if (moveIndex !== log.length - 1) {
+    alert("Please move to the last move to make a move.");
+    return; //user is in detached head state where they cant make moves 😔
+  }
+  let row = number;
+  let col = getNumberFromLetter(letter);
+
+  //no piece is selected currently | selecting new piece
+  if (selectedSquare.length === 0) {
+    selectNewPiece(
+      board,
+      setSelectedSquare,
+      setValidMoves,
+      row,
+      col,
+      isWhiteTurn,
+      isWhite,
+      kingSquare
+    );
+    return;
+  }
+
+  //a piece is currently selected but user clicked another of their own pieces
+  if (
+    board.board[row][col].piece !== null &&
+    board.board[row][col].piece.color ===
+      board.board[selectedSquare[0]][selectedSquare[1]].piece.color
+  ) {
+    selectDifferentPiece(
+      setSelectedSquare,
+      setValidMoves,
+      board,
+      isWhiteTurn,
+      kingSquare,
+      row,
+      col
+    );
+    return;
+  }
+
+  //moving selected piece if a legal square is picked
+  if (isValidMove(board, selectedSquare, row, col, validMoves)) {
+    const newBoard = [...board.board];
+
+    newBoard[row][col].piece =
+      board.board[selectedSquare[0]][selectedSquare[1]].piece;
+
+    //updating moved piece's coordinates
+    newBoard[row][col].piece.letter = LETTERS[col + 1];
+    newBoard[row][col].piece.number = row;
+
+    //setting enPassant if moved piece was a pawn or capturing enPassant'd pawn
+    if (newBoard[row][col].piece.name === "pawn") {
+      let dir = isWhiteTurn ? 1 : -1;
+
+      //did we just enPassant? we will remove that pawn
+      if (
+        board.enPassant.length > 0 &&
+        board.enPassant[0] === row + dir &&
+        board.enPassant[1] === col
+      ) {
+        board.board[board.enPassant[0]][board.enPassant[1]].piece = null;
+        board.enPassant = [];
+      }
+      //did we move to an enPassantable square? set enPassant
+      else {
+        let prevrow = isWhiteTurn ? 6 : 1;
+        let newrow = isWhiteTurn ? 4 : 3;
+
+        if (selectedSquare[0] === prevrow && row == newrow) {
+          board.enPassant = [row, col];
+        }
+      }
+    } else {
+      board.enPassant = [];
+    }
+
+    if (newBoard[row][col].piece.name === "king") {
+      newBoard[row][col].piece.hasMoved = true;
+      isWhiteTurn
+        ? setKingSquare({ ...kingSquare, whiteKing: [row, col] })
+        : setKingSquare({ ...kingSquare, blackKing: [row, col] });
+      //check if it was castling move
+      if (Math.abs(col - selectedSquare[1]) === 2) {
+        //if king is moving more than one square
+        if (col < selectedSquare[1]) {
+          // the castle is queenside
+          newBoard[row][col + 1].piece = newBoard[row][0].piece; //place the rook from queenside next to king
+          newBoard[row][0].piece = null; //remove the rook from queenside
+          newBoard[row][col + 1].piece.letter = LETTERS[col + 2];
+          newBoard[row][col + 1].piece.number = row;
+          newBoard[row][col + 1].piece.hasMoved = true;
+        } else {
+          //the castle is kingside
+          newBoard[row][col - 1].piece = newBoard[row][7].piece; //place the rook from kingside next to king
+          newBoard[row][7].piece = null; //remove the rook from kingside
+          newBoard[row][col - 1].piece.letter = LETTERS[col - 2];
+          newBoard[row][col - 1].piece.number = row;
+          newBoard[row][col - 1].piece.hasMoved = true;
+        }
+      }
+    }
+    if (newBoard[row][col].piece.name === "rook") {
+      newBoard[row][col].piece.hasMoved = true;
+    }
+    newBoard[row][col].piece.letter = LETTERS[col + 1];
+    newBoard[row][col].piece.number = row;
+    newBoard[selectedSquare[0]][selectedSquare[1]].piece = null;
+    //emit move to backend    1 2 3 4 5 6 7 8
+    const fromLetter = LETTERS[selectedSquare[1] + 1];
+    const toLetter = LETTERS[col + 1];
+    const fromTo = `${fromLetter}${8 - selectedSquare[0]}${toLetter}${8 - row}`;
+    socket.sendMessage({
+      action: EmitActions.MOVE_MADE,
+      move: fromTo,
+      gameId: sessionStorage.getItem("gameId"),
+      playerId: sessionStorage.getItem("userId")
+    });
+    // setBoard(newBoard);
+    setSelectedSquare([]);
+    setValidMoves([]);
+    console.log("after set board " + board.enPassant);
+
+    let newTurn = !isWhiteTurn;
+    // setIsWhiteTurn(newTurn);
+    // check if the new player is in checkmate
+    let newPlayerColor = isWhiteTurn ? "black" : "white";
+    let [kingRow, kingCol] = kingSquare[`${newPlayerColor}King`];
+    let king = new King(newPlayerColor, LETTERS[kingCol + 1], kingRow);
+    if (king.isCheckmate(board)) {
+      setHasWon(true);
+      setShowWinner(true);
+      //is, we can display the winner
+    }
+    //update the move log
+    //build new log
+    const notation = convertToChessNotation(
+      newBoard[row][col].piece.name,
+      selectedSquare[0],
+      selectedSquare[1],
+      row,
+      col
+    );
+    const newBoardCopy = newBoard.map((row) => [
+      ...row.map((square) => ({ ...square }))
+    ]);
+    const move = { notation, board: newBoardCopy, isWhiteTurn: newTurn };
+    let newLog = [...log, move];
+    setLog(newLog);
+    setMoveIndex(newLog.length - 1);
+    return;
+  } else {
+    //deselecting current piece since user clicked away
+    setSelectedSquare([]);
+    setValidMoves([]);
+
+    return;
+  }
+};
+
+export default localSelectSquare;
--- components/Board/selectSquare.js
+++ components/Board/selectSquare.js
@@ -1,202 +1,113 @@
 import { getNumberFromLetter } from "../Board/board.js";
 import { LETTERS } from "../Board/board.js";
-import King from "../../pieces/King.js";
-import convertToChessNotation from "../Logger/toChessNotation.js";
-import { EmitActions } from "../../types/Actions.js";
+import {
+  emitMove,
+  handleCheckmate,
+  isValidMove,
+  movePiece,
+  selectDifferentPiece,
+  selectNewPiece,
+  updateEnPassant,
+  updateKingState
+} from "./selectSquareAlgs.js";
 
 const selectSquare = (
   number,
   letter,
   board,
-  setBoard,
   selectedSquare,
   setSelectedSquare,
   isWhiteTurn,
-  setIsWhiteTurn,
   validMoves,
   setValidMoves,
   kingSquare,
   setKingSquare,
   setHasWon,
-  hasWon,
   setShowWinner,
-  setLog,
-  log,
-  setMoveIndex,
-  moveIndex,
   socket,
   isWhite
 ) => {
-  if (moveIndex !== log.length - 1) {
-    alert("Please move to the last move to make a move.");
-    return; //user is in detached head state where they cant make moves 😔
-  }
+  //get the row and column of the square that was clicked
   let row = number;
   let col = getNumberFromLetter(letter);
 
-  //no piece is selected currently | selecting new piece
+  //if no piece is selected currently
   if (selectedSquare.length === 0) {
-    if (board.board[row][col].piece) {
-      if (
-        (board.board[row][col].piece.color === "white" &&
-          isWhiteTurn &&
-          isWhite) ||
-        (board.board[row][col].piece.color === "black" &&
-          !isWhiteTurn &&
-          !isWhite)
-      ) {
-        setSelectedSquare([row, col]);
-        setValidMoves([
-          ...board.board[row][col].piece.getValidMoves(
-            board,
-            isWhiteTurn ? kingSquare.whiteKing : kingSquare.blackKing
-          )
-        ]);
-      }
-    }
+    selectNewPiece(
+      board,
+      setSelectedSquare,
+      setValidMoves,
+      row,
+      col,
+      isWhiteTurn,
+      isWhite,
+      kingSquare
+    );
     return;
   }
 
-  //updating selected piece to newly selected square's piece
+  //if a piece is currently selected but user clicked another of their own pieces
   if (
     board.board[row][col].piece !== null &&
     board.board[row][col].piece.color ===
       board.board[selectedSquare[0]][selectedSquare[1]].piece.color
   ) {
-    setSelectedSquare([row, col]);
-    setValidMoves([
-      ...board.board[row][col].piece.getValidMoves(
-        board,
-        isWhiteTurn ? kingSquare.whiteKing : kingSquare.blackKing
-      )
-    ]);
-    return;
-  }
-
-  //moving selected piece if a legal square is picked
-  if (
-    board.board[selectedSquare[0]][selectedSquare[1]].piece.isValidMove(
-      row,
-      col,
-      validMoves
-    ) === true
-  ) {
-    const newBoard = [...board.board];
-
-    newBoard[row][col].piece =
-      board.board[selectedSquare[0]][selectedSquare[1]].piece;
-
-    //updating moved piece's coordinates
-    newBoard[row][col].piece.letter = LETTERS[col + 1];
-    newBoard[row][col].piece.number = row;
-
-    //setting enPassant if moved piece was a pawn or capturing enPassant'd pawn
-    if (newBoard[row][col].piece.name === "pawn") {
-      let dir = isWhiteTurn ? 1 : -1;
-
-      //did we just enPassant? we will remove that pawn
-      if (
-        board.enPassant.length > 0 &&
-        board.enPassant[0] === row + dir &&
-        board.enPassant[1] === col
-      ) {
-        board.board[board.enPassant[0]][board.enPassant[1]].piece = null;
-        board.enPassant = [];
-      }
-      //did we move to an enPassantable square? set enPassant
-      else {
-        let prevrow = isWhiteTurn ? 6 : 1;
-        let newrow = isWhiteTurn ? 4 : 3;
-
-        if (selectedSquare[0] === prevrow && row == newrow) {
-          board.enPassant = [row, col];
-        }
-      }
-    } else {
-      board.enPassant = [];
-    }
-
-    if (newBoard[row][col].piece.name === "king") {
-      newBoard[row][col].piece.hasMoved = true;
-      isWhiteTurn
-        ? setKingSquare({ ...kingSquare, whiteKing: [row, col] })
-        : setKingSquare({ ...kingSquare, blackKing: [row, col] });
-      //check if it was castling move
-      if (Math.abs(col - selectedSquare[1]) === 2) {
-        //if king is moving more than one square
-        if (col < selectedSquare[1]) {
-          // the castle is queenside
-          newBoard[row][col + 1].piece = newBoard[row][0].piece; //place the rook from queenside next to king
-          newBoard[row][0].piece = null; //remove the rook from queenside
-          newBoard[row][col + 1].piece.letter = LETTERS[col + 2];
-          newBoard[row][col + 1].piece.number = row;
-          newBoard[row][col + 1].piece.hasMoved = true;
-        } else {
-          //the castle is kingside
-          newBoard[row][col - 1].piece = newBoard[row][7].piece; //place the rook from kingside next to king
-          newBoard[row][7].piece = null; //remove the rook from kingside
-          newBoard[row][col - 1].piece.letter = LETTERS[col - 2];
-          newBoard[row][col - 1].piece.number = row;
-          newBoard[row][col - 1].piece.hasMoved = true;
-        }
-      }
-    }
-    if (newBoard[row][col].piece.name === "rook") {
-      newBoard[row][col].piece.hasMoved = true;
-    }
-    newBoard[row][col].piece.letter = LETTERS[col + 1];
-    newBoard[row][col].piece.number = row;
-    newBoard[selectedSquare[0]][selectedSquare[1]].piece = null;
-    //emit move to backend    1 2 3 4 5 6 7 8
-    const fromLetter = LETTERS[selectedSquare[1] + 1];
-    const toLetter = LETTERS[col + 1];
-    const fromTo = `${fromLetter}${8 - selectedSquare[0]}${toLetter}${8 - row}`;
-    socket.sendMessage({
-      action: EmitActions.MOVE_MADE,
-      move: fromTo,
-      gameId: sessionStorage.getItem("gameId"),
-      playerId: sessionStorage.getItem("userId")
-    });
-    // setBoard(newBoard);
-    setSelectedSquare([]);
-    setValidMoves([]);
-    console.log("after set board " + board.enPassant);
-
-    let newTurn = !isWhiteTurn;
-    // setIsWhiteTurn(newTurn);
-    // check if the new player is in checkmate
-    let newPlayerColor = isWhiteTurn ? "black" : "white";
-    let [kingRow, kingCol] = kingSquare[`${newPlayerColor}King`];
-    let king = new King(newPlayerColor, LETTERS[kingCol + 1], kingRow);
-    if (king.isCheckmate(board)) {
-      setHasWon(true);
-      setShowWinner(true);
-      //is, we can display the winner
-    }
-    //update the move log
-    //build new log
-    const notation = convertToChessNotation(
-      newBoard[row][col].piece.name,
-      selectedSquare[0],
-      selectedSquare[1],
+    selectDifferentPiece(
+      setSelectedSquare,
+      setValidMoves,
+      board,
+      isWhiteTurn,
+      kingSquare,
       row,
       col
     );
-    const newBoardCopy = newBoard.map((row) => [
-      ...row.map((square) => ({ ...square }))
-    ]);
-    const move = { notation, board: newBoardCopy, isWhiteTurn: newTurn };
-    let newLog = [...log, move];
-    setLog(newLog);
-    setMoveIndex(newLog.length - 1);
-    return;
-  } else {
-    //deselecting current piece
-    setSelectedSquare([]);
-    setValidMoves([]);
-
     return;
   }
+
+  // if a legal destination square is picked
+  if (isValidMove(board, selectedSquare, row, col, validMoves)) {
+    let newBoard = movePiece(board, row, col, selectedSquare, LETTERS);
+
+    //set enPassant state
+    updateEnPassant(newBoard, row, col, isWhiteTurn, board, selectedSquare);
+    //set king state / handle castling
+    updateKingState(
+      newBoard,
+      row,
+      col,
+      isWhiteTurn,
+      kingSquare,
+      setKingSquare,
+      selectedSquare,
+      LETTERS
+    );
+
+    if (newBoard[row][col].piece.name === "rook") {
+      newBoard[row][col].piece.hasMoved = true;
+    }
+
+    //emit move made to backend
+    emitMove(LETTERS, selectedSquare, row, col, socket);
+
+    // check/handle if the new player is in checkmate
+    const isCheckmate = handleCheckmate(
+      isWhiteTurn,
+      kingSquare,
+      board,
+      setHasWon,
+      setShowWinner,
+      LETTERS
+    ); //TODO: try passing in newBoard instead of board here.
+
+    if (isCheckmate) {
+      console.log("checkmate detected!");
+      //TODO: emit a game over event if it was checkmate. This should be verified in backend.
+    }
+    //update the move log
+  }
+  //deselect current piece since user clicked away or finished moving their piece
+  setSelectedSquare([]);
+  setValidMoves([]);
 };
 
 export default selectSquare;
