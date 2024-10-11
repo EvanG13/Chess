dc0e7663d8f148222133b8ee3a95764d3d78cbf9
CleveyP <clevelandplonsey@gmail.com>
2024-10-11 02:52:16 -0700

    create handleSendChat helper function

---
+++ components/RightSideBar/handleSendChat.js
@@ -0,0 +1,12 @@
+import { EmitActions } from "../../types/Actions";
+
+const handleSendChat = (socket, message) => {
+  socket.sendMessage({
+    action: EmitActions.CHAT_MESSAGE,
+    username: sessionStorage.getItem("username"),
+    chatMessage: message,
+    userId: sessionStorage.getItem("userId")
+  });
+};
+
+export default handleSendChat;
