9a22038c99525232586dd908707ec5574c2ebdd7
CleveyP <clevelandplonsey@gmail.com>
2024-10-11 02:51:35 -0700

    Craete ChatMessage component

---
+++ components/RightSideBar/ChatMessage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { View, Text, StyleSheet } from "react-native";
+
+const ChatMessage = ({ messageObject }) => {
+  return (
+    <View>
+      <Text style={styles.message}>{messageObject.message}</Text>
+    </View>
+  );
+  //TODO: add datetime stamp to the message
+};
+
+const styles = StyleSheet.create({
+  message: {}
+});
+
+export default ChatMessage;
