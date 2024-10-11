d24497e906725cf273ed119c88ac43f6d4a59842
CleveyP <clevelandplonsey@gmail.com>
2024-10-11 03:10:38 -0700

    fix: correct some Action enum values in accordance with backend

--- types/Actions.js
+++ types/Actions.js
@@ -3,7 +3,8 @@ const Actions = Object.freeze({
   GAME_OVER: "GAME_OVER",
   MOVE_MADE: "MOVE_MADE",
   CHAT_MESSAGE: "CHAT_MESSAGE",
-  GAME_CREATED: "GAME_CREATED"
+  GAME_CREATED: "GAME_CREATED",
+  OFFER_DRAW: "OFFER_DRAW",
 });
 
 export const EmitActions = Object.freeze({
