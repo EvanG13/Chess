fa747cab022c451ec2c9d3bdecd8d988317343f6
CleveyP <clevelandplonsey@gmail.com>
2024-10-17 22:40:49 -0700

    fix websocket loading the right env

--- components/axiosInstance.js
+++ components/axiosInstance.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { BACKEND_BASE_URL } from '@env'; // Assuming you're using react-native-dotenv for env variables
+import { BACKEND_BASE_URL } from '@env'; 
 const url = BACKEND_BASE_URL;
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: url, // Your base URL, typically from an env variable
 });
+
+
 
 export default axiosInstance;
