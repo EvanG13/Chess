d8d5a5a832540ce80c0f2a50f65a6c461eff3dec
CleveyP <clevelandplonsey@gmail.com>
2024-11-09 02:01:37 -0800

    create game over modal

---
+++ components/gameOverModal/GameOverModal.jsx
@@ -0,0 +1,44 @@
+import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { EmitActions } from "../../types/Actions.js";
+import styles from "../PromptModal/PromptModalStyles.js";
+
+const GameOverModal = ({ isVisible, setIsVisible, message, socket }) => {
+
+    const handleRematch = () => {};
+
+    const handleNewGame = () => {};
+
+  if (!isVisible) return null; 
+
+  return (
+    <View style={styles.modalContainer}>
+      <SafeAreaView style={styles.modal}>
+        <TouchableOpacity
+          style={styles.closeButton}
+          onPress={() => setIsVisible(false)}
+        >
+          <Text>X</Text>
+        </TouchableOpacity>
+        <Text style={styles.messageText}>{message}</Text>
+        <View style={styles.buttonBox}>
+        <TouchableOpacity
+          onPress={handleRematch}
+        >
+          <Text style={styles.buttonText}>
+            Rematch
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleNewGame}
+        >
+          <Text style={styles.buttonText}>
+            New Game
+          </Text>
+        </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default GameOverModal;
