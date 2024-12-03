b131aa8dcda49dc990624315e3a18b2d04332e7a
EvanG13 <emgiampaoli@gmail.com>
2024-12-02 16:05:26 -0800

    chore: setup import alias (#93)

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+defaultConfig.resolver.alias = {
+  "@": "./*"
+};
+
 module.exports = defaultConfig;
