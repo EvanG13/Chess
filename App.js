dfeb78b36308dd6396fc54a116e219110cfd0073
EvanG13 <emgiampaoli@gmail.com>
2024-12-07 16:26:41 -0800

    chore: add safe area provider (#99)

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import Board from "./components/Board/Board.jsx";
 import { NavigationContainer } from "@react-navigation/native";
@@ -12,47 +11,45 @@ import Profile from "./screens/ProfileScreen/Profile";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import ReviewGameScreen from "./screens/GameReviewScreen/ReviewGameScreen";
 import GameStat from "@/screens/CategoryStats/GameStat";
+import {
+  SafeAreaProvider,
+  initialWindowMetrics
+} from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        style={styles.container}
-        screenOptions={{
-          headerShown: false
-        }}
-      >
-        <Stack.Screen name="Chess" component={HomeScreen} />
-        <Stack.Screen
-          name="localGame"
-          component={Board}
-          initialParams={{ isLocalGame: true }}
-        />
-        <Stack.Screen name="onlineGame" component={OnlineGameScreen} />
-        <Stack.Screen name="reviewGame" component={ReviewGameScreen} />
-        <Stack.Screen name="login" component={Login} />
-        <Stack.Screen name="register" component={Register} />
-        <Stack.Screen name="profile" component={Profile} />
-        <Stack.Screen name="stats" component={GameStat} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Chess" component={HomeScreen} />
+          <Stack.Screen
+            name="localGame"
+            component={Board}
+            initialParams={{ isLocalGame: true }}
+          />
+          <Stack.Screen name="onlineGame" component={OnlineGameScreen} />
+          <Stack.Screen name="reviewGame" component={ReviewGameScreen} />
+          <Stack.Screen name="login" component={Login} />
+          <Stack.Screen name="register" component={Register} />
+          <Stack.Screen name="profile" component={Profile} />
+          <Stack.Screen name="stats" component={GameStat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
-  },
-  button: {
-    backgroundColor: "black"
-  },
-  text: {
-    color: "white"
+    alignItems: "center",
+    backgroundColor: "#121212"
   }
 });
